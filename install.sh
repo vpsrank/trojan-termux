f6f27736371921d9f3662451d26ddd1d8678383a
vpsrank <48700915+vpsrank@users.noreply.github.com>
2019-05-10 10:15:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,35 +14,3 @@ make install
 #trojan配置
 cp -f /data/data/com.termux/files/usr/share/doc/trojan/examples/client.json-example /data/data/com.termux/files/usr/etc/trojan/config.json
 read -p "输入trojan服务器域名:" domain && read -p "输入trojan密码:" password && sed -i "s/password1/$password/g;s/example.com/$domain/g" /data/data/com.termux/files/usr/etc/trojan/config.json
-
-#clash
-pkg install golang wget -y
-cd ~
-go get -u -ldflags "-s -w" github.com/Dreamacro/clash && chmod +x go/bin/clash && mv go/bin/clash /data/data/com.termux/files/usr/bin/
-
-#clash配置
-mkdir ~/.config/clash ; wget https://raw.githubusercontent.com/ConnersHua/Profiles/master/Clash/Global.yml -O ~/.config/clash/config.yml
-sed -i '/^# HTTP 代理端口/,/# 规则$/d ' ~/.config/clash/config.yml
-sed -i '1iport: 1090\
-allow-lan: false\
-mode: Rule\
-log-level: silent\
-external-controller: 127.0.0.1:9090\
-Proxy:\
-- { name: "trojan", type: socks5, server: 127.0.0.1, port: 1080 }\
-Proxy Group:\
-- { name: "PROXY", type: select, proxies: ["trojan"] }\
-# 白名单模式 PROXY，黑名单模式 DIRECT\
-- { name: "FINAL", type: select, proxies: ["PROXY", "DIRECT"] }\
-- { name: "MEDIA", type: select, proxies: ["PROXY"] }\
-- { name: "HIJACKING", type: select, proxies: ["DIRECT", "REJECT"] }\
-' ~/.config/clash/config.yml
-
-#开机启动,需要安装termux:boot插件APP
-mkdir -p ~/.termux/boot/
-echo "termux-wake-lock
-trojan & clash &" >> ~/.termux/boot/trojan
-
-#清理
-#pkg uninstall clang cmake boost-dev golang -y
-#apt autoremove -y
